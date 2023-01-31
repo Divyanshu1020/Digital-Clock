05b20122f17cef67a48ff9af86cd1e20bd1b5410
Divyanshu <divyanshuvishwakarma222@gmail.com>
2023-01-31 22:27:10 +0530

    Add a button to change time 12 to 24 hours

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 let hours = document.getElementById("hours")
 let minutes = document.getElementById("minutes")
 let seconds = document.getElementById("seconds")
-let AmPm = document.getElementById("am")
+const links = document.querySelector(".links");
+const btn = document.querySelector("#btn");
 
 let change = document.getElementById("btn")
+let am = "AM"
 
 function updateClock(){
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
-    let am = "AM"
-    
+    if(h > 12 && links.classList.contains("am")){
+        h = h - 12;
+        am = "PM"
+    }
     hours.textContent = h;
     minutes.textContent = m;
     seconds.textContent = s;
-    AmPm.textContent = am;
     setTimeout(()=>{
         updateClock()
     }, 1000)
@@ -23,6 +26,15 @@ function updateClock(){
 updateClock()
 
 change.addEventListener("click", function(){
-    
-    
-})
+
+    links.classList.toggle("am")
+    if(links.classList.contains("am")){
+        links.textContent = am
+        btn.textContent = "24 Hour"
+
+    }
+    else{
+        links.textContent = ""
+        btn.textContent = "12 Hour"
+    }
+} )
